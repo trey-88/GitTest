6a967ae253a8cf8666555efca04d2037f406b1be
Timur Khusainov <thusainov88@gmail.com>
2022-02-10 00:01:25 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+// 1
+// function multiply() {
+//   if (arguments.length === 0) return 0;
+//   let res = 1;
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (!isNaN(arguments[i])) {
+//       res *= arguments[i];
+//     }
+//   }
+//   console.log(res);
+//   return res;
+// }
+
+// multiply(1, 2, 3, 4);
+
+// function multiply() {
+//   if (arguments.length === 0) return;
+//   let res = 1;
+//   for (let i = 0; i < arguments.length; i++) {
+//     if (!isNaN(arguments[i])) {
+//       res *= arguments[i];
+//     }
+//   }
+//   console.log(res);
+//   return res;
+// }
+
+// multiply(1, 2, 2, 4);
+
+// 3
+// function getCodeStringFromText(string) {
+//   let res = '';
+//   for (i = 0; i < string.length; i++) {
+//     res += string[i] + ' ';
+//   }
+//   console.log(res);
+//   return res;
+// }
+
+// getCodeStringFromText('Hello');
+
+// 4
+
+// function gameCheckNumber(num) {
+//   if (num < 1 || num > 10) {
+//     console.log('Функция принимает исло от 1 до 10');
+//     return;
+//   }
+//   const random = Math.ceil(Math.random() * 10);
+//   if (random === num) {
+//     console.log('Вы выиграли');
+//   } else {
+//     console.log(`Вы не угадали ваше число ${num} а выпало число ${random}`);
+//   }
+// }
+
+// gameCheckNumber(5);
+
+// function gameCheckNumber(num) {
+//   if (num < 1 || num > 10) {
+//     console.log('Функция принимает значения от 1 до 10');
+//     return;
+//   }
+//   const random = Math.ceil(Math.random() * 10);
+//   if (random === num) {
+//     console.log('Вы выиграли');
+//   } else {
+//     console.log(`Вы не угадали ваше число ${num} а выпало число ${random}`);
+//   }
+// }
+
+// gameCheckNumber(5);
+
+// const numArr = [1, 2, 3, 4, 5];
+// let value;
+
+// value = 'hello world'.split(' ');
+
+// console.log(value);
+
+function filterRange (arr, a, b) {
+
+}
+
+filterRange ();
